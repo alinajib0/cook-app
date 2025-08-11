95309befa8693f54e6c965f09b0d5da928c4169f
alinajib0 <alinajibhegazy@proton.me>
2025-08-11 12:07:23 +0300

    make project ready to work

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,4 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <>this is project</>
-)
+createRoot(document.getElementById("root")!).render(<>this is project</>);
